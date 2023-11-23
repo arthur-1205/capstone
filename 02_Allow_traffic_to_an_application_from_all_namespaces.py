920ab03a216724bb04727929f518970f2e46c4e7
Adrien_1205 <63574039+arthur-1205@users.noreply.github.com>
2023-11-23 23:12:35 +0700

    Update and rename 02_Limit_traffic_to_an_application.py to 02_Allow_traffic_to_an_application_from_all_namespaces.py
    Update 02_Allow_traffic_to_an_application_from_all_namespaces.py

--- 02_Allow_traffic_to_an_application_from_all_namespaces.py
+++ 02_Allow_traffic_to_an_application_from_all_namespaces.py
@@ -3,42 +3,35 @@ import subprocess
 import kubernetes
 import json
 
-# Nhập giá trị cho app label từ bàn phím
-new_app_label = input("Enter value for App label: ")
-# namespace = selected_namespace
+
+
 with open("namespace.json", "r") as config_file:
-    config = json.load(config_file)
-namespace = config["namespace"]
+    namespace_config = json.load(config_file)
+    namespace = namespace_config["namespace"]
+    selected_label = namespace_config.get("label", {}).get("app", "")
 
 # Tạo đối tượng NetworkPolicy với app label mới
 network_policy = {
     "kind": "NetworkPolicy",
     "apiVersion": "networking.k8s.io/v1",
     "metadata": {
-        "name": "api-allow",
-        "namespace": namespace
+        "namespace": namespace,
+        "name": selected_label.lower() + "-allow-all-namespaces"
     },
     "spec": {
         "podSelector": {
             "matchLabels": {
-                "app": new_app_label,  
-                "role": "api"
-            }
+                "app": selected_label,
+            },
         },
         "ingress": [
             {
                 "from": [
-                    {
-                        "podSelector": {
-                            "matchLabels": {
-                                "app": new_app_label,
-                            }
-                        }
-                    }
+                    {"namespaceSelector": {}}
                 ]
             }
         ]
-    }   
+    }
 }
 
     
@@ -61,16 +54,18 @@ def apply_kubernetes_yaml(yaml_file_path):
         print("An error occurred:", str(e))
 
 while True:
-            # Menu
         print("1. Execute")
         print("2. Export to a yaml file with a name of your choice")
         choice = input("Select an option (1 or 2): ")
 
         if choice == "1":
-            Limit_traffic_to_an_application_yaml = yaml.dump(network_policy, default_flow_style=False)
-            with open("Limit_traffic_to_an_application_yaml", "w") as temp_file:
-                temp_file.write(Limit_traffic_to_an_application_yaml)
-            apply_kubernetes_yaml('Limit_traffic_to_an_application_yaml')
+            yaml_string = yaml.dump(network_policy, default_flow_style=False)
+            new_yaml_filename = f"Allow-traffic-to-an-application-from-all-namespaces.yaml"
+
+            with open(new_yaml_filename, "w") as temp_file:
+                temp_file.write(yaml_string)
+
+            apply_kubernetes_yaml(new_yaml_filename)
 
             break
         elif choice == "2":
@@ -80,5 +75,5 @@ while True:
             print(f"Saved to {filename}.yaml!")
             break
         else:
-            print("Invalid selection. Please select again!")
+            print("Invalid selection. Please select again.")
             
